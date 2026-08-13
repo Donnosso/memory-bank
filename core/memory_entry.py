dd12f2fce39754f4c8ccf8f391ea907b4b371f51
David Imakhai <donnossodon@gmail.com>
2026-08-13 10:52:47 +0100

    Store multiple memories using a list of dictionaries

--- core/memory_entry.py
+++ core/memory_entry.py
@@ -1,6 +1,26 @@
-title = input("Title: ")
-date = input("Date: ")
-mood = input("Mood: ")
-journal = input("Journal: ")
+memories = []
+add_another = "y"
 
-print (f'\nMemory saved:{title} ({date}) - Mood: {mood}\n{journal}')
+while add_another == "y":
+    title = input("Title: ")
+    date = input("Date: ")
+    mood = input("Mood: ")
+    journal = input("Journal: ")
+
+#print (f'\nMemory saved:{title} ({date}) - Mood: {mood}\n{journal}')
+
+    memory = {
+        'title': title,
+        'date': date,
+        'mood': mood,
+        'journal': journal
+    }
+
+    memories.append(memory)
+
+
+    add_another = input("Add another memory(y/n): ")
+
+### PRINTS THE WHOLE MEMORY COLLECTION ###
+for m in memories:
+    print(f'\n{m['title']}\n{m['date']}\n{m['mood']}\n{m['journal']}')
