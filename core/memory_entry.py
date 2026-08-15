4f6484cd47c01ed8431855eeb41cd08da74601fc
David Imakhai <donnossodon@gmail.com>
2026-08-15 10:58:44 +0100

    Functions that capture memory and display memories

--- core/memory_entry.py
+++ core/memory_entry.py
@@ -1,26 +1,32 @@
 memories = []
-add_another = "y"
-
-while add_another == "y":
-    title = input("Title: ")
-    date = input("Date: ")
-    mood = input("Mood: ")
-    journal = input("Journal: ")
-
-#print (f'\nMemory saved:{title} ({date}) - Mood: {mood}\n{journal}')
-
-    memory = {
-        'title': title,
-        'date': date,
-        'mood': mood,
-        'journal': journal
-    }
-
-    memories.append(memory)
 
 
-    add_another = input("Add another memory(y/n): ")
+def capture_memory():
+    add_another = "y"
+    while add_another == "y":
+        title = input("Title: ")
+        date = input("Date: ")
+        mood = input("Mood: ")
+        journal = input("Journal: ")
 
-### PRINTS THE WHOLE MEMORY COLLECTION ###
-for m in memories:
-    print(f'\n{m['title']}\n{m['date']}\n{m['mood']}\n{m['journal']}')
+        memory = {
+            'title': title,
+            'date': date,
+            'mood': mood,
+            'journal': journal
+        }
+        add_another = input("Add another memory(y/n): ")
+        memories.append(memory)
+
+    return memory
+
+
+def display_memories(memories):
+    ### PRINTS THE WHOLE MEMORY COLLECTION ###
+    for m in memories:
+        print(f'\n{m['title']}\n{m['date']}\n{m['mood']}\n{m['journal']}')
+
+
+capture_memory()
+display_memories(memories)
+
