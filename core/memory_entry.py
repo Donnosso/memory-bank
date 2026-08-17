c87dc230ca9dc383f1839b758313eb36203daa57
David Imakhai <donnossodon@gmail.com>
2026-08-17 19:34:23 +0100

    Fix search to check all memories before reporting no match

--- core/memory_entry.py
+++ core/memory_entry.py
@@ -1,13 +1,12 @@
 memories = []
 
-
 def capture_memory():
     add_another = "y"
     while add_another == "y":
-        title = input("Title: ")
+        title = input("Title: ").lower()
         date = input("Date: ")
-        mood = input("Mood: ")
-        journal = input("Journal: ")
+        mood = input("Mood: ").lower()
+        journal = input("Journal: ").lower()
 
         memory = {
             'title': title,
@@ -22,11 +21,32 @@ def capture_memory():
 
 
 def display_memories(memories):
-    ### PRINTS THE WHOLE MEMORY COLLECTION ###
+    if not memories == None:
+        print("No memories yet. Why don't you create some..")
+        return
     for m in memories:
         print(f'\n{m['title']}\n{m['date']}\n{m['mood']}\n{m['journal']}')
 
 
-capture_memory()
+def search_memories(memories, keyword):
+    searched_result = []
+    for m in memories:
+        if keyword in m['title'] or keyword in m['journal']:
+            searched_result.append(m)
+    ###TELLS THE USER IF THE SEARCH_KEY IS NOT AVAILABLE AFTER THE LOOP CHECK###
+    if not searched_result:
+        print(f"You don't have this '{keyword}' memory yet.")
+        return
+    ###RETURNS THE SEARCHED MEMORY DICTIONARY AFTER THE LOOP CHECK###
+    return searched_result
+
+
+
+#capture_memory()
+
 display_memories(memories)
 
+search_key = input("What do you wish to recollect...? ")
+result = search_memories(memories, search_key)
+if result:
+    display_memories(result)
